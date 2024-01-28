c4d89da8c3c4f8713e5e64a15239a71c209bff59
Shane Howe <shanehowe100@hotmail.com>
2024-01-28 21:04:20 +0000

    Add navigation to SignUpScreen and implement SignIn functionality

--- routes/AuthStack.tsx
+++ routes/AuthStack.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SignInScreen } from '../screens/SignInScreen';
+import { SignUpScreen } from '../screens/SignUpScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +10,13 @@ export const AuthStack = () => {
             <Stack.Screen 
                 name="SignIn"
                 component={SignInScreen}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Stack.Screen
+                name='SignUp'
+                component={SignUpScreen}
                 options={{
                     headerShown: false,
                 }}
