32da029d631f21db2724af9d000b2538ce046cf1
Shane Howe <shanehowe100@hotmail.com>
2024-01-28 16:43:29 +0000

    Removed unused imports and styles

--- App.tsx
+++ App.tsx
@@ -1,28 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { useColorScheme } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import { Router } from './routes/Router';
 import { AuthProvidor } from './contexts/AuthContext';
+import { theme } from './theme/theme';
 
 import React from 'react';
 
 export default function App() {
 
+  const colorScheme = useColorScheme();
+
+  const paperTheme = colorScheme === "dark" ? theme.dark : theme.dark;
+
   return (
     <AuthProvidor>
-      <PaperProvider>
+      <PaperProvider theme={paperTheme}>
           <Router />
           <StatusBar style="auto" />
       </PaperProvider>
     </AuthProvidor>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
