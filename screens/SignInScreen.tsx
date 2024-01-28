0d27118fc0310cb7a947932c7a0365ec27f04aad
Shane Howe <shanehowe100@hotmail.com>
2024-01-28 16:45:13 +0000

    break code into components

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,18 +1,41 @@
-import { View, Text, Alert } from "react-native";
+import { View, SafeAreaView } from "react-native";
+import { Avatar, useTheme } from "react-native-paper";
 import { StyleSheet } from "react-native";
+import { SignInForm } from "../components/Auth/SignInForm";
+import { AuthButtons } from "../components/Auth/AuthButtons";
 
 export const SignInScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Sign In Screen</Text>
-        </View>
-    );
-}
+  const theme = useTheme();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.background }}>
+      <View
+        style={[styles.container, { backgroundColor: theme.colors.background }]}
+      >
+        <Avatar.Icon
+          style={{
+            backgroundColor: theme.colors.primary,
+            alignSelf: "center",
+            flex: 0,
+            marginBottom: 20,
+          }}
+          size={100}
+          icon="lock"
+        />
+        <SignInForm />
+
+        <AuthButtons />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingLeft: 20,
+    paddingRight: 20,
+    justifyContent: "center",
+  },
 });
