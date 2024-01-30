77147c7228d45572b1a537188bd0cd2c8a0ced3b
Shane Howe <shanehowe100@hotmail.com>
2024-01-30 21:50:15 +0000

    Refactor PasswordInput component

--- components/PasswordInput.tsx
+++ components/PasswordInput.tsx
@@ -1,33 +1,38 @@
-import React from 'react';
-import { TextInput } from 'react-native-paper';
+import React from "react";
+import { TextInput } from "react-native-paper";
 
 interface PasswordInputProps {
-    label: string;
-    onChangeText: (text: string) => void;
-    mode: 'flat' | 'outlined';
-    style?: object;
+  label: string;
+  onChangeText: (text: string) => void;
+  mode: "flat" | "outlined";
+  style?: object;
 }
 
-export const PasswordInput = ({ label, onChangeText, mode, style }: PasswordInputProps) => {
-    const [showPassword, setShowPassword] = React.useState(false);
+export const PasswordInput = ({
+  label,
+  onChangeText,
+  mode,
+  style,
+}: PasswordInputProps) => {
+  const [showPassword, setShowPassword] = React.useState(false);
 
-    const toggleShowPassword = () => setShowPassword(!showPassword);
+  const toggleShowPassword = () => setShowPassword(!showPassword);
 
-    style = style ? style : { width: '100%' };
+  style = style ? style : { width: "100%" };
 
-    return (
-        <TextInput
-            onChangeText={onChangeText}
-            label={label}
-            mode={mode}
-            secureTextEntry={showPassword}
-            style={style}
-            right={
-                <TextInput.Icon
-                    icon={showPassword ? "eye" : "eye-off"}
-                    onPress={toggleShowPassword}
-                />
-            }
+  return (
+    <TextInput
+      onChangeText={onChangeText}
+      label={label}
+      mode={mode}
+      secureTextEntry={!showPassword}
+      style={style}
+      right={
+        <TextInput.Icon
+          icon={showPassword ? "eye-off" : "eye"}
+          onPress={toggleShowPassword}
         />
-    );
+      }
+    />
+  );
 };
