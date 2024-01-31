1b0556573ab3d79a00c13c34267253944a3fdc52
Shane Howe <shanehowe100@hotmail.com>
2024-01-31 23:53:53 +0000

    Add QueryClientProvider to App component

--- App.tsx
+++ App.tsx
@@ -3,9 +3,12 @@ import { useColorScheme } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import { Router } from './routes/Router';
 import { AuthProvidor } from './contexts/AuthContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { theme } from './theme/theme';
 
 import React from 'react';
+
+const queryClient = new QueryClient();
 
 export default function App() {
 
@@ -15,10 +18,12 @@ export default function App() {
 
   return (
     <AuthProvidor>
+      <QueryClientProvider client={queryClient} >
       <PaperProvider theme={paperTheme}>
           <Router />
           <StatusBar style="auto" />
       </PaperProvider>
+      </QueryClientProvider>
     </AuthProvidor>
   );
 };
