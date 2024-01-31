63725272c34ccffd8e7a4c5e60302a4edc0692de
Shane Howe <shanehowe100@hotmail.com>
2024-01-31 23:52:51 +0000

    Add WorkoutFolderItem component

---
+++ components/Workouts/WorkoutFolderItem.tsx
@@ -0,0 +1,38 @@
+import { Divider, List, useTheme } from "react-native-paper";
+import { WorkoutFolder } from "../../types";
+import { Fragment } from "react";
+
+interface WorkoutFolderItemProps {
+  folder: WorkoutFolder;
+  showDivider: boolean;
+}
+
+export const WorkoutFolderItem = ({
+  folder,
+  showDivider,
+}: WorkoutFolderItemProps) => {
+  const theme = useTheme();
+
+  return (
+    <Fragment key={folder.id}>
+      <List.Item
+        onPress={() => console.log(`Pressed ${folder.name}`)}
+        title={folder.name}
+        left={(props) => (
+          <List.Icon {...props} icon="folder" color={theme.colors.primary} />
+        )}
+        right={(props) => <List.Icon {...props} icon="chevron-right" />}
+      />
+
+      {showDivider && (
+        <Divider
+          style={{
+            width: "100%",
+            alignSelf: "center",
+            backgroundColor: theme.colors.outline,
+          }}
+        />
+      )}
+    </Fragment>
+  );
+};
