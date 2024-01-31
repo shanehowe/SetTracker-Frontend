aaf1f2c1b814db37f24541546c4ad917d84a77fa
Shane Howe <shanehowe100@hotmail.com>
2024-01-31 23:52:47 +0000

    Refactor WorkoutFolderList component

--- components/Workouts/WorkoutFolderList.tsx
+++ components/Workouts/WorkoutFolderList.tsx
@@ -1,36 +1,46 @@
-import { View, StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import { WorkoutFolder } from "../../types";
-import { WorkoutFolderCard } from "./WorkoutFolderCard";
-
-const workoutFolders: WorkoutFolder[] = [
-  {
-    id: "1",
-    name: "Chest Day",
-    exercises: ["Bench Press", "Incline Bench Press", "Decline Bench Press"],
-  },
-  {
-    id: "2",
-    name: "Back and Biceps",
-    exercises: ["Deadlift", "Pull Ups", "Rows"],
-  },
-];
+import { WorkoutFolderItem } from "./WorkoutFolderItem";
+import { useQuery } from "@tanstack/react-query";
+import workoutFolderService from "../../services/workoutFolders";
+import { Card, List } from "react-native-paper";
 
 export const WorkoutFolderList = () => {
+  const {
+    isLoading,
+    error,
+    data: workoutFolders,
+  } = useQuery<WorkoutFolder[]>({
+    queryKey: ["workoutFolders"],
+    queryFn: workoutFolderService.getAll,
+  });
+
+  if (isLoading) return <Text>Loading...</Text>;
+
+  if (error) return <Text>{error.message}</Text>;
+
   return (
-    <View style={styles.workoutFolderList}>
-      {workoutFolders.map((folder) => (
-        <WorkoutFolderCard key={folder.id} folder={folder} />
-      ))}
-    </View>
+    <Card
+      mode="contained"
+      style={styles.card}
+    >
+      <List.Section>
+        {workoutFolders!.map((folder, index) => (
+          <WorkoutFolderItem
+            key={folder.id}
+            folder={folder}
+            showDivider={index !== workoutFolders!.length - 1}
+          />
+        ))}
+      </List.Section>
+    </Card>
   );
 };
 
 const styles = StyleSheet.create({
-  workoutFolderList: {
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 25,
-    width: "100%",
-    gap: 15,
-  },
+  card: {
+    width: "90%",
+    alignSelf: "center",
+    marginTop: 10,
+  }
 });
