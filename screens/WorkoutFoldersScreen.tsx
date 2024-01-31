cbc5b8804b7e963f5be40e07f255cc3f013c9e05
Shane Howe <shanehowe100@hotmail.com>
2024-01-31 23:53:20 +0000

    Update WorkoutFoldersScreen component

--- screens/WorkoutFoldersScreen.tsx
+++ screens/WorkoutFoldersScreen.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { ScreenProps } from "../interfaces";
 import { Button, Text, useTheme } from "react-native-paper";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
 import { AddWorkoutFolderFAB } from "../components/Buttons/AddWorkoutFolderFAB";
 import { WorkoutFolderList } from "../components/Workouts/WorkoutFolderList";
+import { AddWorkoutFolderModal } from "../components/Workouts/AddWorkoutFolderModal";
 
 export const WorkoutFoldersScreen = ({ navigation }: ScreenProps) => {
+  const [modalVisible, setModalVisible] = React.useState(false);
   const theme = useTheme();
 
+  const showModal = () => setModalVisible(true);
+  const hideModal = () => setModalVisible(false);
+
   return (
-    <View style={{ backgroundColor: theme.colors.background, flex: 1 }}>
-      <AddWorkoutFolderFAB onPress={() => {}} />
+    <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
+    <ScrollView contentContainerStyle={[{ backgroundColor: theme.colors.background }]}>
+      <AddWorkoutFolderModal visible={modalVisible} hideModal={hideModal} />
+      
       <View style={styles.allExercisesButtonContainer}>
         <Button mode="contained" style={styles.allExercisesButton}>
           All Exercises
@@ -22,6 +29,8 @@ export const WorkoutFoldersScreen = ({ navigation }: ScreenProps) => {
         </Text>
         <WorkoutFolderList />
       </View>
+    </ScrollView>
+    <AddWorkoutFolderFAB onPress={showModal} />
     </View>
   );
 };
