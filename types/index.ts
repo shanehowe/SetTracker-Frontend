d6e09073184757038a1a2050ed5fc50d3b7dc82c
Shane Howe <shanehowe100@hotmail.com>
2024-02-01 21:29:47 +0000

    Add SnackbarState and SnackbarAction types

--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,14 @@ export type WorkoutFolder = {
     name: string;
     exercises: string[];
 }
+
+export type SnackbarState = {
+    open: boolean;
+    message: string;
+    severity: 'success' | 'info' | 'error' | undefined;
+};
+
+export type SnackbarAction = {
+    type: 'OPEN_SNACKBAR' | 'CLOSE_SNACKBAR';
+    payload?: SnackbarState;
+};
