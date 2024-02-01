69cd459b9c41ea756840c41c71e79928df1b3b08
Shane Howe <shanehowe100@hotmail.com>
2024-02-01 21:30:52 +0000

    Add SnackbarService interface for displaying success, info, and error messages

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -3,3 +3,10 @@ import { NavigationProp } from "@react-navigation/native";
 export interface ScreenProps {
   navigation: NavigationProp<any, any>;
 }
+
+export interface SnackbarService {
+  success: (message: string) => void;
+  info: (message: string) => void;
+  error: (message: string) => void;
+  close: () => void;
+}
