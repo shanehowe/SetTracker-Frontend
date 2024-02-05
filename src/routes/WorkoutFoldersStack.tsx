2524a341c912219af7b6d387e4d3ffa1fbfe5b9d
Shane Howe <shanehowe100@hotmail.com>
2024-02-05 22:24:26 +0000

    Refactor WorkoutFoldersStack to use createStackNavigator

--- src/routes/WorkoutFoldersStack.tsx
+++ src/routes/WorkoutFoldersStack.tsx
@@ -1,15 +1,25 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import { WorkoutFoldersScreen } from "../screens/workouts/WorkoutFoldersScreen";
+import { AllExercisesScreen } from "../screens/workouts/AllExercisesScreen";
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export const WorkoutFoldersStack = () => {
   return (
-    <Stack.Navigator initialRouteName="WorkoutFolders">
+    <Stack.Navigator
+      screenOptions={{
+        gestureEnabled: true
+      }}
+      initialRouteName="WorkoutFolders">
       <Stack.Screen
         name="WorkoutFolders"
         component={WorkoutFoldersScreen}
         options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="AllExercises"
+        component={AllExercisesScreen}
+        options={{ headerShown: false}}
       />
     </Stack.Navigator>
   );
