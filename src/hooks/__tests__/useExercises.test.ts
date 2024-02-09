081d1bfbcf9701de3090c8d838d75c93a417d848
Shane Howe <shanehowe100@hotmail.com>
2024-02-09 15:55:44 +0000

    Refactor useExercises hook and exerciseService

--- src/hooks/__tests__/useExercises.test.ts
+++ src/hooks/__tests__/useExercises.test.ts
@@ -4,13 +4,13 @@ import { queryClientWrapper, mockedQueryClient } from "../../test-utils";
 import exerciseService from "../../services/exercises";
 
 const testExerciseList = [
-  { name: 'legs' },
-  { name: 'back' },
-  { name: 'back1' },
-  { name: 'shoulder press' },
+  { id: "1", name: "legs" },
+  { id: "2", name: "back" },
+  { id: "3", name: "back1" },
+  { id: "4", name: "shoulder press" },
 ];
 
-jest.spyOn(exerciseService, 'getAll').mockImplementation(() => {
+jest.spyOn(exerciseService, "getAll").mockImplementation(() => {
   return testExerciseList;
 });
 
@@ -18,10 +18,10 @@ afterEach(() => mockedQueryClient.clear());
 
 describe("useExercises", () => {
   it("returns the full list when filter is empty", async () => {
-    const { result } = renderHook(
-      useExercises,
-      { wrapper: queryClientWrapper, initialProps: "" }
-    );
+    const { result } = renderHook(useExercises, {
+      wrapper: queryClientWrapper,
+      initialProps: "",
+    });
 
     await waitFor(() => expect(result.current.isLoading).toBeFalsy());
 
@@ -29,38 +29,38 @@ describe("useExercises", () => {
   });
 
   it("returns a filtered list when given a non empty filter", async () => {
-    const { result } = renderHook(
-      useExercises,
-      { wrapper: queryClientWrapper, initialProps: "back" }
-    );
+    const { result } = renderHook(useExercises, {
+      wrapper: queryClientWrapper,
+      initialProps: "back",
+    });
 
     await waitFor(() => expect(result.current.isLoading).toBeFalsy());
 
     expect(result.current.exercises).toEqual([
-      { name: "back" },
-      { name: "back1" }
+      { id: "2", name: "back" },
+      { id: "3", name: "back1" },
     ]);
   });
 
   it("filter is not case sensitive", async () => {
-    const { result } = renderHook(
-      useExercises,
-      { wrapper: queryClientWrapper, initialProps: "bAcK" }
-    );
+    const { result } = renderHook(useExercises, {
+      wrapper: queryClientWrapper,
+      initialProps: "bAcK",
+    });
 
     await waitFor(() => expect(result.current.isLoading).toBeFalsy());
 
     expect(result.current.exercises).toEqual([
-      { name: "back" },
-      { name: "back1" }
+      { id: "2", name: "back" },
+      { id: "3", name: "back1" },
     ]);
   });
 
   it("returns an empty array when filter does not match anything", async () => {
-    const { result } = renderHook(
-      useExercises,
-      { wrapper: queryClientWrapper, initialProps: "this is definitely not in the list" }
-    );
+    const { result } = renderHook(useExercises, {
+      wrapper: queryClientWrapper,
+      initialProps: "this is definitely not in the list",
+    });
 
     await waitFor(() => expect(result.current.isLoading).toBeFalsy());
 
@@ -69,14 +69,14 @@ describe("useExercises", () => {
 
   it("can handle errors and returns an error message", async () => {
     const mockError = new Error("error");
-    jest.spyOn(exerciseService, 'getAll').mockImplementationOnce(() => {
+    jest.spyOn(exerciseService, "getAll").mockImplementationOnce(() => {
       return Promise.reject(mockError) as any;
     });
 
-    const { result } = renderHook(
-      useExercises,
-      { wrapper: queryClientWrapper, initialProps: "" }
-    );
+    const { result } = renderHook(useExercises, {
+      wrapper: queryClientWrapper,
+      initialProps: "",
+    });
 
     await waitFor(() => expect(result.current.isLoading).toBeFalsy());
 
@@ -86,13 +86,13 @@ describe("useExercises", () => {
 
   it("returns an empty array when an error is thrown", async () => {
     const mockError = new Error("error");
-    jest.spyOn(exerciseService, 'getAll').mockImplementationOnce(() => {
+    jest.spyOn(exerciseService, "getAll").mockImplementationOnce(() => {
       return Promise.reject(mockError) as any;
     });
 
     const { result } = renderHook(useExercises, {
       wrapper: queryClientWrapper,
-      initialProps: ""
+      initialProps: "",
     });
 
     await waitFor(() => expect(result.current.isLoading).toBeFalsy());
--- src/services/exercises.ts
+++ src/services/exercises.ts
@@ -3,16 +3,22 @@ import { Exercise } from "../types";
 
 const getAll = (): Exercise[] => {
   return [
-    { name: "Bench" },
-    { name: "Squat" },
-    { name: "Shoulder Press" },
-    { name: "Leg extension" },
-    { name: "Bent Over Row" },
+    { id: "1", name: "Bench" },
+    { id: "2", name: "Squat" },
+    { id: "3", name: "Shoulder Press" },
+    { id: "4", name: "Leg extension" },
+    { id: "5", name: "Bent Over Row" },
   ];
+};
+
+const createCustom = (exerciseName: string): Exercise => {
+  const id = Math.floor(Math.random() * 1000).toString();
+  return { id, name: exerciseName };
 };
 
 const exerciseService = {
   getAll,
+  createCustom,
 };
 
 export default exerciseService;
