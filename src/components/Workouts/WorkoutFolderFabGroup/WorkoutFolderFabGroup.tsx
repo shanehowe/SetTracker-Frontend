b7004fca11413161bbf74faf29fa44fde92b2a0e
Shane Howe <shanehowe100@hotmail.com>
2024-02-10 20:39:41 +0000

    Add WorkoutFolderFabGroup component and its test

---
+++ src/components/Workouts/WorkoutFolderFabGroup/WorkoutFolderFabGroup.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { FAB, Portal, useTheme } from "react-native-paper";
+import { StyleSheet } from "react-native";
+
+export const WorkoutFolderFabGroup = () => {
+  const [state, setState] = useState({ open: false });
+  const theme = useTheme();
+
+  const onStateChange = ({ open }: { open: boolean }) => setState({ open });
+  const { open } = state;
+
+  return (
+    <Portal>
+      <FAB.Group
+        label="Folder Options"
+        style={styles.fabStyle}
+        testID="workout-folder-fab-group"
+        open={open}
+        visible
+        icon={open ? "close" : "folder-edit"}
+        actions={[
+          {
+            icon: "delete",
+            label: "Delete",
+            onPress: () => console.log("Delete"),
+            color: theme.colors.error,
+          },
+          {
+            icon: "rename-box",
+            label: "Rename folder",
+            onPress: () => console.log("Rename"),
+          },
+          {
+            icon: "plus-circle",
+            label: "Add Exercises",
+            onPress: () => console.log("Add Exercises"),
+          },
+        ]}
+        onStateChange={onStateChange}
+      />
+    </Portal>
+  );
+};
+
+const styles = StyleSheet.create({
+  fabStyle: {
+    position: "absolute",
+    bottom: 50,
+  },
+});
---
+++ src/components/Workouts/WorkoutFolderFabGroup/__tests__/WorkoutFolderFabGroup.test.tsx
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react-native";
+import { WorkoutFolderFabGroup } from "../WorkoutFolderFabGroup";
+import { AllTheProviders } from "../../../../test-utils";
+
+describe("WorkoutFolderFabGroup", () => {
+  it("should render", () => {
+    const { getByTestId } = render(
+      <WorkoutFolderFabGroup />,
+      { wrapper: AllTheProviders }
+    );
+
+    expect(getByTestId("workout-folder-fab-group")).toBeTruthy();
+  });
+});
