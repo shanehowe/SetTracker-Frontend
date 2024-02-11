334c7b173c7994f488b0b32872fcd53adf01a06a
Shane Howe <shanehowe100@hotmail.com>
2024-02-11 22:09:20 +0000

    Refactor exercise retrieval in exercises.ts

--- src/services/exercises.ts
+++ src/services/exercises.ts
@@ -1,14 +1,16 @@
 import { Exercise } from "../types";
 
 
+const exercises: Exercise[] = [
+  { id: "1", name: "Bench" },
+  { id: "2", name: "Squat" },
+  { id: "3", name: "Shoulder Press" },
+  { id: "4", name: "Leg extension" },
+  { id: "5", name: "Bent Over Row" },
+];
+
 const getAll = (): Exercise[] => {
-  return [
-    { id: "1", name: "Bench" },
-    { id: "2", name: "Squat" },
-    { id: "3", name: "Shoulder Press" },
-    { id: "4", name: "Leg extension" },
-    { id: "5", name: "Bent Over Row" },
-  ];
+  return exercises;
 };
 
 const createCustom = (exerciseName: string): Exercise => {
