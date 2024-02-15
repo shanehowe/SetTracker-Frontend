df48357e0ec430cd32f49bff5ddf0376159d9e35
Shane Howe <shanehowe100@hotmail.com>
2024-02-15 18:44:40 +0000

    Add useRenameFolderMutation hook

---
+++ src/hooks/useRenameFolderMutation.ts
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import workoutFolderService from "../services/workoutFolders";
+import { WorkoutFolder } from "../types";
+
+export const useRenameFolderMutation = (
+  folderId: string,
+  onSuccessCallback: (renamedFolder: WorkoutFolder) => void,
+  onErrorCallback: () => void
+) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (name: string) => workoutFolderService.rename(folderId, name),
+    onSuccess: (data) => {
+      queryClient.setQueryData(["folder", folderId], data);
+      queryClient.invalidateQueries({
+        queryKey: ["workoutFolders"],
+      });
+      onSuccessCallback(data);
+    },
+    onError: (error) => {
+      onErrorCallback();
+    },
+  });
+};
