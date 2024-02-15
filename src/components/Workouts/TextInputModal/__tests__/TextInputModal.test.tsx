1d748365764fd39b296610f6935bed8f4e183a04
Shane Howe <shanehowe100@hotmail.com>
2024-02-15 17:33:34 +0000

    Reformat to keep linter happy

--- src/components/Workouts/TextInputModal/__tests__/TextInputModal.test.tsx
+++ src/components/Workouts/TextInputModal/__tests__/TextInputModal.test.tsx
@@ -75,45 +75,45 @@ describe("TextInputModal", () => {
     expect(onChageText).toHaveBeenCalledWith("new text");
   });
 
-    it("calls onSubmit correctly", () => {
-        const onSubmit = jest.fn();
-        const { getByTestId } = render(
-        <TextInputModal
-            visible={true}
-            onDismiss={() => {}}
-            title="Title"
-            placeholder="Placeholder"
-            onChageText={() => {}}
-            onSubmit={onSubmit}
-            testID="test-id"
-        />,
-        { wrapper: AllTheProviders }
-        );
-    
-        const button = getByTestId("add-button");
-        fireEvent.press(button);
-    
-        expect(onSubmit).toHaveBeenCalled();
-    });
+  it("calls onSubmit correctly", () => {
+    const onSubmit = jest.fn();
+    const { getByTestId } = render(
+      <TextInputModal
+        visible={true}
+        onDismiss={() => {}}
+        title="Title"
+        placeholder="Placeholder"
+        onChageText={() => {}}
+        onSubmit={onSubmit}
+        testID="test-id"
+      />,
+      { wrapper: AllTheProviders }
+    );
 
-    it("calls onDismiss correctly", () => {
-        const onDismiss = jest.fn();
-        const { getByTestId } = render(
-        <TextInputModal
-            visible={true}
-            onDismiss={onDismiss}
-            title="Title"
-            placeholder="Placeholder"
-            onChageText={() => {}}
-            onSubmit={() => {}}
-            testID="test-id"
-        />,
-        { wrapper: AllTheProviders }
-        );
-    
-        const button = getByTestId("cancel-modal-button");
-        fireEvent.press(button);
-    
-        expect(onDismiss).toHaveBeenCalled();
-    });
+    const button = getByTestId("add-button");
+    fireEvent.press(button);
+
+    expect(onSubmit).toHaveBeenCalled();
+  });
+
+  it("calls onDismiss correctly", () => {
+    const onDismiss = jest.fn();
+    const { getByTestId } = render(
+      <TextInputModal
+        visible={true}
+        onDismiss={onDismiss}
+        title="Title"
+        placeholder="Placeholder"
+        onChageText={() => {}}
+        onSubmit={() => {}}
+        testID="test-id"
+      />,
+      { wrapper: AllTheProviders }
+    );
+
+    const button = getByTestId("cancel-modal-button");
+    fireEvent.press(button);
+
+    expect(onDismiss).toHaveBeenCalled();
+  });
 });
