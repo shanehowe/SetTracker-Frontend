435c485c6222a90e49194bafdc35d038038b04a5
Shane Howe <shanehowe100@hotmail.com>
2024-02-15 15:43:08 +0000

    Add getById function to exerciseService

--- src/services/exercises.ts
+++ src/services/exercises.ts
@@ -13,13 +13,23 @@ const getAll = (): Exercise[] => {
   return exercises;
 };
 
+const getById = (exerciseId: string): Exercise => {
+  const exercise =  exercises.find((exercise) => exercise.id === exerciseId);
+  if (!exercise) {
+    throw new Error("Exercise not found");
+  }
+  return exercise;
+};
+
 const createCustom = (exerciseName: string): Exercise => {
   const id = Math.floor(Math.random() * 1000).toString();
+  exercises.push({ id, name: exerciseName });
   return { id, name: exerciseName };
 };
 
 const exerciseService = {
   getAll,
+  getById,
   createCustom,
 };
 
