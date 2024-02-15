58fac186e1dd6e274a327bfdaf8f72388be2e260
Shane Howe <shanehowe100@hotmail.com>
2024-02-15 15:43:20 +0000

    Add Exercise import and update exercises in workoutFolders.ts

--- src/services/workoutFolders.ts
+++ src/services/workoutFolders.ts
@@ -1,4 +1,5 @@
-import { WorkoutFolder } from "../types";
+import { Exercise, WorkoutFolder } from "../types";
+import exerciseService from "./exercises";
 
 // Mock data for now
 const workoutFolders: WorkoutFolder[] = [
@@ -6,18 +7,18 @@ const workoutFolders: WorkoutFolder[] = [
     id: "1",
     name: "Chest Day",
     exercises: [
-      { id: "1", name: "Bent Over Row" },
-      { id: "2", name: "Pull Ups" },
-      { id: "3", name: "Bicep Curls" },
+      { id: "1", name: "Bench" },
+      { id: "2", name: "Squat" },
+      { id: "3", name: "Shoulder Press" },
     ],
   },
   {
     id: "2",
     name: "Back and Biceps",
     exercises: [
-      { id: "1", name: "Bent Over Row" },
-      { id: "2", name: "Pull Ups" },
-      { id: "3", name: "Bicep Curls" },
+      { id: "1", name: "Bench" },
+      { id: "2", name: "Squat" },
+      { id: "3", name: "Shoulder Press" },
     ],
   },
 ];
@@ -43,10 +44,22 @@ const getById = (folderId: string) => {
   return folder;
 };
 
+const updateExercises = (folderId: string, exercises: string[]) => {
+  const folder = workoutFolders.find((folder) => folder.id === folderId);
+  if (!folder) {
+    throw new Error("Folder not found");
+  }
+  folder.exercises = exercises.map((exercise) => {
+    return exerciseService.getById(exercise);
+  });
+  return folder;
+};
+
 const workoutFolderService = {
   getAll,
   create,
   getById,
+  updateExercises,
 };
 
 export default workoutFolderService;
