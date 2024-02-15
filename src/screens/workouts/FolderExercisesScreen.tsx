765da22a31e1422cc2b78067fdebb063db84acb4
Shane Howe <shanehowe100@hotmail.com>
2024-02-15 18:45:03 +0000

    Add rename folder functionality

--- src/screens/workouts/FolderExercisesScreen.tsx
+++ src/screens/workouts/FolderExercisesScreen.tsx
@@ -7,6 +7,10 @@ import { FolderExercises } from "../../components/Workouts/FolderExercises/Folde
 import { useFolder } from "../../hooks/useFolder";
 import { useFocusEffect } from "@react-navigation/native";
 import { useState } from "react";
+import { TextInputModal } from "../../components/Workouts/TextInputModal/TextInputModal";
+import { useRenameFolderMutation } from "../../hooks/useRenameFolderMutation";
+import { useSnack } from "../../contexts/SnackbarContext";
+import { WorkoutFolder } from "../../types";
 
 interface FolderExercisesScreenProps extends ScreenProps {
   route: {
@@ -22,10 +26,32 @@ export const FolderExercisesScreen = ({
   navigation,
 }: FolderExercisesScreenProps) => {
   const [visible, setVisible] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [newFolderName, setNewFolderName] = useState("");
+
   const theme = useTheme();
+  const snack = useSnack();
 
   const folderId = route.params.folderId;
   const { isError, error, isLoading, folder } = useFolder(folderId);
+
+  const onSuccessCallback = (updatedFolder: WorkoutFolder) => {
+    snack.success(`Folder renamed to ${updatedFolder.name}!`);
+  };
+  const onErrorCallback = () => {
+    snack.error("Failed to rename folder");
+  };
+
+  const renameFolderMutation = useRenameFolderMutation(
+    folderId,
+    onSuccessCallback,
+    onErrorCallback
+  );
+
+  const handleRenameFolder = () => {
+    renameFolderMutation.mutate(newFolderName);
+    setModalVisible(false);
+  }
 
   useFocusEffect(() => {
     setVisible(true);
@@ -47,7 +73,20 @@ export const FolderExercisesScreen = ({
     >
       <FolderHeading folderName={folder!.name} />
       <FolderExercises exercises={folder!.exercises} />
-      <WorkoutFolderFabGroup visible={visible} folderId={folderId} />
+      <WorkoutFolderFabGroup
+        visible={visible}
+        folderId={folderId}
+        handleRenameFolderClick={() => setModalVisible(true)}
+      />
+      <TextInputModal
+        visible={modalVisible}
+        placeholder="Folder Name"
+        title="Rename Folder"
+        onDismiss={() => setModalVisible(false)}
+        onSubmit={handleRenameFolder}
+        onChageText={(text) => setNewFolderName(text)}
+        testID="rename-folder-modal"
+      />
     </ScrollView>
   );
 };
