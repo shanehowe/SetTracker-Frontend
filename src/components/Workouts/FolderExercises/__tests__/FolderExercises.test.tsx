4d88132b3e280a6378191896112cdffc17c5d8a3
Shane Howe <shanehowe100@hotmail.com>
2024-02-19 22:21:37 +0000

    Add exercises prop to FolderExercises component

--- src/components/Workouts/FolderExercises/__tests__/FolderExercises.test.tsx
+++ src/components/Workouts/FolderExercises/__tests__/FolderExercises.test.tsx
@@ -1,9 +1,22 @@
 import { render } from "@testing-library/react-native";
 import { FolderExercises } from "../FolderExercises";
+import { AllTheProviders } from "../../../../test-utils";
+import { Exercise } from "../../../../types";
+
+const exercises: Exercise[] = [
+  { id: "1", name: "Bench" },
+  { id: "2", name: "Squat" },
+  { id: "3", name: "Shoulder Press" },
+  { id: "4", name: "Leg extension" },
+  { id: "5", name: "Bent Over Row" },
+];
 
 describe("FolderExercises", () => {
   it("renders", () => {
-    const { getByTestId } = render(<FolderExercises />);
+    const { getByTestId } = render(
+    <FolderExercises folderId="1" exercises={exercises} />,
+    { wrapper: AllTheProviders}
+    );
 
     const container = getByTestId("folder-exercises-container");
     expect(container).toBeTruthy();
