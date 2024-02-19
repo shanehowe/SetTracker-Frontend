bd19fb226dbaa14768dd031ab8a833eb45bdc87e
Shane Howe <shanehowe100@hotmail.com>
2024-02-19 22:24:29 +0000

    Add tests for rendering exercise list component

--- src/components/Workouts/FolderExercises/__tests__/FolderExercises.test.tsx
+++ src/components/Workouts/FolderExercises/__tests__/FolderExercises.test.tsx
@@ -21,4 +21,24 @@ describe("FolderExercises", () => {
     const container = getByTestId("folder-exercises-container");
     expect(container).toBeTruthy();
   });
+
+  it("renders exercise list component when exercises prop is not empty", () => {
+    const { getByTestId } = render(
+      <FolderExercises folderId="1" exercises={exercises} />,
+      { wrapper: AllTheProviders}
+      );
+  
+      const component = getByTestId("exercise-list");
+      expect(component).toBeTruthy();
+  });
+
+  it("renders empty exercise list component when exercises prop is empty", () => {
+    const { getByTestId } = render(
+      <FolderExercises folderId="1" exercises={[]} />,
+      { wrapper: AllTheProviders}
+      );
+  
+      const component = getByTestId("empty-folder-exercise-list-container");
+      expect(component).toBeTruthy();
+  });
 });
