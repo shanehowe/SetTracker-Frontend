d687ce698cb8783f51545b61c04aed32429dab7b
Shane Howe <shanehowe100@hotmail.com>
2024-02-20 22:36:50 +0000

    Remove showDivider prop from WorkoutFolderItem component

--- src/components/Workouts/WorkoutFolderItem/WorkoutFolderItem.tsx
+++ src/components/Workouts/WorkoutFolderItem/WorkoutFolderItem.tsx
@@ -1,16 +1,14 @@
-import { Divider, List, useTheme } from "react-native-paper";
+import { List, useTheme } from "react-native-paper";
 import { WorkoutFolder, RootStackParamList } from "../../../types";
 import { Fragment } from "react";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 
 interface WorkoutFolderItemProps {
   folder: WorkoutFolder;
-  showDivider: boolean;
 }
 
 export const WorkoutFolderItem = ({
   folder,
-  showDivider,
 }: WorkoutFolderItemProps) => {
   const theme = useTheme();
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
@@ -36,17 +34,6 @@ export const WorkoutFolderItem = ({
         right={(props) => <List.Icon {...props} icon="chevron-right" />}
         testID="workout-folder-item"
       />
-
-      {showDivider && (
-        <Divider
-          style={{
-            width: "100%",
-            alignSelf: "center",
-            backgroundColor: theme.colors.outline,
-          }}
-          testID="workout-folder-divider"
-        />
-      )}
     </Fragment>
   );
 };
