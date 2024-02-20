159c818c02fcef213a891e2188bcb7d34adfb71e
Shane Howe <shanehowe100@hotmail.com>
2024-02-20 22:38:03 +0000

    Add basic jsx for SetHistoryScreen for testing

--- src/screens/workouts/SetHistoryScreen.tsx
+++ src/screens/workouts/SetHistoryScreen.tsx
@@ -1,3 +1,10 @@
-export const SetHistoryScreen = () => {
-  
+import { Button } from "react-native-paper";
+import { ScreenProps } from "../../interfaces";
+
+export const SetHistoryScreen = ({ navigation }: ScreenProps) => {
+  return (
+    <Button onPress={navigation.goBack}>
+        Go Back
+    </Button>
+  )
 };
