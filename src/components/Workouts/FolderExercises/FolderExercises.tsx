1cb8baf58f22e867f6fce0389cf678b42786b294
Shane Howe <shanehowe100@hotmail.com>
2024-02-20 22:36:44 +0000

    Add navigation to SetHistory screen in FolderExercises component

--- src/components/Workouts/FolderExercises/FolderExercises.tsx
+++ src/components/Workouts/FolderExercises/FolderExercises.tsx
@@ -1,7 +1,8 @@
 import { Card } from "react-native-paper";
 import { ExerciseList } from "../ExerciseList/ExerciseList";
-import { Exercise } from "../../../types";
+import { Exercise, RootStackParamList } from "../../../types";
 import { EmptyFolderExerciseList } from "../EmptyFolderExercisesList/EmptyFolderExerciseList";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
 
 interface FolderExercisesProps {
   exercises: Exercise[];
@@ -9,11 +10,16 @@ interface FolderExercisesProps {
 }
 
 export const FolderExercises = ({ exercises, folderId }: FolderExercisesProps) => {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
+  const goToSetHistoryScreen = () => {
+    navigation.navigate("SetHistory");
+  };
 
   let content;
 
   if (exercises && exercises.length > 0) {
-    content = <ExerciseList exercises={exercises}/>;
+    content = <ExerciseList exercises={exercises} handleExerciseItemOnPress={goToSetHistoryScreen}/>;
   } else {
     content = <EmptyFolderExerciseList folderId={folderId} />;
   }
