b84d616c547090fe12c14213345742dcca499208
Shane Howe <shanehowe100@hotmail.com>
2024-02-20 22:36:16 +0000

    Refactor App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { useColorScheme } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
-import { Router } from './routes/Router';
-import { AuthProvidor } from './contexts/AuthContext';
-import { SnackbarProvider } from './contexts/SnackbarContext';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { theme } from './theme/theme';
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import { Router } from "./routes/Router";
+import { AuthProvidor } from "./contexts/AuthContext";
+import { SnackbarProvider } from "./contexts/SnackbarContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { theme } from "./theme/theme";
 
-import React from 'react';
+import React from "react";
 
 const queryClient = new QueryClient();
 
 export default function App() {
-
   const colorScheme = useColorScheme();
 
   const paperTheme = colorScheme === "dark" ? theme.dark : theme.light;
 
   return (
     <AuthProvidor>
-      <QueryClientProvider client={queryClient} >
-      <PaperProvider theme={paperTheme}>
-        <SnackbarProvider>
-          <Router />
-          <StatusBar style="auto" />
-        </SnackbarProvider>
-      </PaperProvider>
+      <QueryClientProvider client={queryClient}>
+        <PaperProvider theme={paperTheme}>
+          <GestureHandlerRootView style= {{ flex: 1 }}>
+            <SnackbarProvider>
+              <Router />
+              <StatusBar style="auto" />
+            </SnackbarProvider>
+          </GestureHandlerRootView>
+        </PaperProvider>
       </QueryClientProvider>
     </AuthProvidor>
   );
-};
+}
