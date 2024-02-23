41177df486ab804d6aa835e49da192152270f7fb
Shane Howe <shanehowe100@hotmail.com>
2024-02-23 23:23:14 +0000

    Add ScrollView and useTheme hook to SetHistoryScreen

--- src/screens/workouts/SetHistoryScreen.tsx
+++ src/screens/workouts/SetHistoryScreen.tsx
@@ -1,10 +1,13 @@
-import { Button } from "react-native-paper";
+import { Button, useTheme } from "react-native-paper";
 import { ScreenProps } from "../../interfaces";
+import { ScrollView } from "react-native";
 
 export const SetHistoryScreen = ({ navigation }: ScreenProps) => {
+  const theme = useTheme();
+
   return (
-    <Button onPress={navigation.goBack}>
-        Go Back
-    </Button>
-  )
+    <ScrollView contentContainerStyle={{ flex: 1, backgroundColor: theme.colors.background }}>
+      <Button onPress={navigation.goBack}>Go Back</Button>
+    </ScrollView>
+  );
 };
