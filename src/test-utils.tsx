8b900cd39bd4fcb2d7ec08a572b421181271b0a7
Shane Howe <shanehowe100@hotmail.com>
2024-02-25 23:47:51 +0000

    Add bottom sheet and gesture handler support

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -4,38 +4,41 @@ import { AuthProvidor } from "./contexts/AuthContext";
 import { PaperProvider } from "react-native-paper";
 import { SnackbarProvider } from "./contexts/SnackbarContext";
 import { NavigationContainer } from "@react-navigation/native";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 interface WrappperArgs {
-  children: React.ReactNode
-};
+  children: React.ReactNode;
+}
 
 export const mockedQueryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: false,
-    }
-  }
+    },
+  },
 });
 
 export const queryClientWrapper = ({ children }: WrappperArgs) => {
-
   return (
-    <QueryClientProvider client={mockedQueryClient} >
-      { children }
+    <QueryClientProvider client={mockedQueryClient}>
+      {children}
     </QueryClientProvider>
   );
-}
+};
 
 export const AllTheProviders = ({ children }: WrappperArgs) => {
   return (
     <AuthProvidor>
-      <QueryClientProvider client={mockedQueryClient} >
+      <QueryClientProvider client={mockedQueryClient}>
         <PaperProvider>
-          <NavigationContainer>
-          <SnackbarProvider>
-            { children }
-          </SnackbarProvider>
-          </NavigationContainer>
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <BottomSheetModalProvider>
+              <NavigationContainer>
+                <SnackbarProvider>{children}</SnackbarProvider>
+              </NavigationContainer>
+            </BottomSheetModalProvider>
+          </GestureHandlerRootView>
         </PaperProvider>
       </QueryClientProvider>
     </AuthProvidor>
