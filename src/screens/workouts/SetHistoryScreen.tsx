13d08bd2157eca91f6a896797ff69eb333101c1a
Shane Howe <shanehowe100@hotmail.com>
2024-02-25 23:48:37 +0000

    Refactor SetHistoryScreen component and add AddSetBottomSheet

--- src/screens/workouts/SetHistoryScreen.tsx
+++ src/screens/workouts/SetHistoryScreen.tsx
@@ -1,39 +1,68 @@
-import { Button, Card, useTheme, Text } from "react-native-paper";
+import { FAB, useTheme } from "react-native-paper";
 import { ScreenProps } from "../../interfaces";
 import { ScrollView, StyleSheet, View } from "react-native";
-import { SetItem } from "../../components/Workouts/SetItem/SetItem";
 import { SetItemGroup } from "../../components/Workouts/SetItemGroup/SetItemGroup";
+import { useCallback, useRef } from "react";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
+import { AddSetBottomSheet } from "../../components/Workouts/AddSetBottomSheet/AddSetBottomSheet";
 
 const sets = [
   {
     id: "1",
     timeStamp: new Date().toLocaleTimeString(),
     weight: 50,
-    reps: 10
+    reps: 10,
   },
   {
     id: "2",
     timeStamp: new Date().toLocaleTimeString(),
     weight: 50,
-    reps: 10
-  }
-]
+    reps: 10,
+  },
+];
 
 export const SetHistoryScreen = ({ navigation }: ScreenProps) => {
   const theme = useTheme();
+  const ref = useRef<BottomSheetModal>(null);
+
+  const handlePresentModalPress = useCallback(() => {
+    ref.current?.present();
+  }, []);
+
+  const handleModalClose = useCallback(() => {
+    ref.current?.close();
+  }, []);
+
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+  }, []);
 
   return (
-    <ScrollView contentContainerStyle={{ flex: 1, backgroundColor: theme.colors.background }}>
-      <Button onPress={navigation.goBack}>Go Back</Button>
+    <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
+      <ScrollView
+        contentContainerStyle={{ backgroundColor: theme.colors.background }}
+      >
         <SetItemGroup sets={sets} date={new Date().toDateString()} />
-    </ScrollView>
+      </ScrollView>
+      <AddSetBottomSheet
+        ref={ref}
+        handleSheetChanges={handleSheetChanges}
+        handleModalClose={handleModalClose}
+      />
+      <FAB
+        icon={"plus"}
+        style={styles.fabStyle}
+        onPress={handlePresentModalPress}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  card: {
-    width: "90%",
+  fabStyle: {
+    position: "absolute",
+    margin: 16,
+    bottom: 20,
     alignSelf: "center",
-    marginTop: 10,
   },
 });
