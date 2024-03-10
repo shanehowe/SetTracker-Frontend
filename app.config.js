19844cd432722cc15478709f67c6266e5faa6bc8
Shane Howe <shanehowe100@hotmail.com>
2024-03-10 23:26:42 +0000

    change app.json to app.config.js

---
+++ app.config.js
@@ -0,0 +1,34 @@
+import "dotenv/config";
+
+export default {
+  name: "set-tracker",
+  slug: "set-tracker",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./src/assets/icon.png",
+  userInterfaceStyle: "automatic",
+  splash: {
+    image: "./src/assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  plugins: ["expo-apple-authentication"],
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: false,
+    usesAppleSignIn: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./src/assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+  },
+  web: {
+    favicon: "./src/assets/favicon.png",
+  },
+  extras: {
+    apiUrl: process.env.API_URL,
+    env: process.env.ENV,
+  },
+};
