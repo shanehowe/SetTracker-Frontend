2cbbf3a222d77e383c272ca0d5ad81671beb698e
Shane Howe <shanehowe100@hotmail.com>
2024-03-23 17:43:32 +0000

    Add user authentication functionality

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import storage from "../utils/storage";
+import authService from "../services/auth";
+import { AxiosError } from "axios";
+import { User } from "../types";
 
 interface AuthContextProps {
     children: React.ReactNode;
@@ -6,24 +10,49 @@ interface AuthContextProps {
 
 type AuthContextType = {
     isSignedIn: boolean;
-    signIn: (username: string, password: string) => void;
+    signIn: (provider: string, token: string) => void;
     signOut: () => void;
+    user: User | null;
 };
 
 const AuthContext = React.createContext<AuthContextType>({
     isSignedIn: false,
-    signIn: () => {},
+    signIn: (provider: string, token: string) => {},
     signOut: () => {},
+    user: null,
 });
 
 const AuthProvidor: React.FC<AuthContextProps> = ({ children }) => {
     const [isSignedIn, setIsSignedIn] = React.useState(false);
+    const [user, setUser] = React.useState<User | null>(null);
+    
+    React.useEffect(() => {
+        const checkAuth = async () => {
+            const user: User = JSON.parse(await storage.get("loggedInUser") || "{}");
+            if (user) {
+                setIsSignedIn(true);
+                setUser(user);
+                authService.setToken(user.token);
+            }
+        };
 
-    const signIn = (username: string, password: string) => {
-        setIsSignedIn(true);
+        checkAuth();
+    }, []);
+
+    const signIn = async (provider: string, token: string) => {
+        try {
+            const response = await authService.signIn(provider, token);
+            if (response) {
+                await storage.set("loggedInUser", JSON.stringify(response));
+                setIsSignedIn(true);
+            }
+        } catch (error: AxiosError | any) {
+            console.error(error.code, error.message);
+        }
     };
 
-    const signOut = () => {
+    const signOut = async () => {
+        await storage.remove("token");
         setIsSignedIn(false);
     };
 
@@ -31,6 +60,7 @@ const AuthProvidor: React.FC<AuthContextProps> = ({ children }) => {
         isSignedIn,
         signIn,
         signOut,
+        user,
     };
 
     return (
