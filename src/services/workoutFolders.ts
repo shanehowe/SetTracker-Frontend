328ea26c5357a744f4d6412032708ee2f40561e0
Shane Howe <shanehowe100@hotmail.com>
2024-03-31 13:56:14 +0100

    Refactor workoutFolders service to use API calls

--- src/services/workoutFolders.ts
+++ src/services/workoutFolders.ts
@@ -1,80 +1,47 @@
-import { Exercise, WorkoutFolder } from "../types";
-import exerciseService from "./exercises";
+import { WorkoutFolder } from "../types";
+import {token} from "./auth";
+import Constants from "expo-constants";
+import axios from "axios";
 
-// Mock data for now
-const workoutFolders: WorkoutFolder[] = [
-  {
-    id: "1",
-    name: "Chest Day",
-    exercises: [
-      { id: "1", name: "Bench" },
-      { id: "2", name: "Squat" },
-      { id: "3", name: "Shoulder Press" },
-    ],
-  },
-  {
-    id: "2",
-    name: "Back and Biceps",
-    exercises: [
-      { id: "1", name: "Bench" },
-      { id: "2", name: "Squat" },
-      { id: "3", name: "Shoulder Press" },
-    ],
-  },
-];
+const API_URL = Constants.expoConfig?.extra?.apiUrl;
 
-const getAll = () => {
-  return workoutFolders;
+const getAll = async () => {
+  const headers = {
+    "Authorization": `Bearer ${token}`
+  };
+  const response = await axios.get<WorkoutFolder[]>(
+    `${API_URL}/workout-folders/`,
+    { headers }
+  );
+  return response.data;
 };
 
-const create = (folderName: string) => {
-  const newFolder: WorkoutFolder = {
-    id: Math.floor(Math.random() * 1000).toString(),
-    name: folderName,
-    exercises: [],
+const create = async (folderName: string) => {
+  const headers = {
+    "Authorization": `Bearer ${token}`
   };
-  return newFolder;
+  const payload = {
+    name: folderName
+  };
+
+  const response = await axios.post<WorkoutFolder>(
+    `${API_URL}/workout-folders/`,
+    payload,
+    { headers }
+  );
+  return response.data;
 };
 
 const getById = (folderId: string) => {
-  const folder = workoutFolders.find((folder) => folder.id === folderId);
-  if (!folder) {
-    throw new Error("Folder not found");
-  }
-  return folder;
 };
 
 const updateExercises = (folderId: string, exercises: string[]) => {
-  const folder = workoutFolders.find((folder) => folder.id === folderId);
-  if (!folder) {
-    throw new Error("Folder not found");
-  }
-  folder.exercises = exercises.map((exercise) => {
-    try {
-      return exerciseService.getById(exercise);
-    } catch (error) {
-      return exerciseService.createCustom(exercise);
-    }
-  });
-  return folder;
 };
 
 const remove = (folderId: string) => {
-  const folderIndex = workoutFolders.findIndex((folder) => folder.id === folderId);
-  if (folderIndex === -1) {
-    throw new Error("Folder not found");
-  }
-  workoutFolders.splice(folderIndex, 1);
-  return true;
 };
 
 const rename = (folderId: string, name: string) => {
-  const index = workoutFolders.findIndex((folder) => folder.id === folderId);
-  if (index === -1) {
-    throw new Error("Folder not found");
-  }
-  workoutFolders[index].name = name;
-  return workoutFolders[index];
 };
 
 const workoutFolderService = {
