295e8c1084852b00142ff99aa3aa3a1d4903ddc5
Shane Howe <shanehowe100@hotmail.com>
2024-04-07 21:46:07 +0100

    Update useCheckboxGroup to support custom comparison function

--- src/hooks/useCheckboxGroup.ts
+++ src/hooks/useCheckboxGroup.ts
@@ -1,13 +1,16 @@
 import { useState } from "react";
 
-export const useCheckboxGroup = <T>(alreadySelected: T[] = []) => {
+export const useCheckboxGroup = <T>(
+  alreadySelected: T[] = [],
+  compare: (a: T, b: T) => boolean
+) => {
   const [selected, setSelected] = useState<T[]>([...alreadySelected]);
 
   const handleSelect = (value: T) => {
-    if (selected.includes(value)) {
-      setSelected(selected.filter((item) => item !== value));
+    if (selected.some((item) => compare(item, value))) {
+      setSelected(selected.filter((item) => !compare(item, value)));
     } else {
-      setSelected([...selected, value]);
+      setSelected(selected.concat(value));
     }
   };
 
