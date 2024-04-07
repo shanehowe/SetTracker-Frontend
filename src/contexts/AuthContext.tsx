dead4118c80e204c3871de0f8982acd23ff3b75a
Shane Howe <shanehowe100@hotmail.com>
2024-04-07 21:45:57 +0100

    Refactor AuthContext component in AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -5,68 +5,69 @@ import { AxiosError } from "axios";
 import { User } from "../types";
 
 interface AuthContextProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 type AuthContextType = {
-    signIn: (provider: string, token: string) => void;
-    signOut: () => void;
-    user: User | null;
+  signIn: (provider: string, token: string) => void;
+  signOut: () => void;
+  user: User | null;
 };
 
 const AuthContext = React.createContext<AuthContextType>({
-    signIn: (provider: string, token: string) => {},
-    signOut: () => {},
-    user: null,
+  signIn: (provider: string, token: string) => {},
+  signOut: () => {},
+  user: null,
 });
 
 const AuthProvidor: React.FC<AuthContextProps> = ({ children }) => {
-    const [user, setUser] = React.useState<User | null>(null);
-    
-    React.useEffect(() => {
-        const checkAuth = async () => {
-            try {
-                const user = await storage.get("loggedInUser");
-                if (user) {
-                    setUser(JSON.parse(user));
-                }
-            } catch (error) {
-                // do nothing
-            }
-        };
+  const [user, setUser] = React.useState<User | null>(null);
 
-        checkAuth();
-    }, []);
-
-    const signIn = async (provider: string, token: string) => {
-        try {
-            const response = await authService.signIn(provider, token);
-            if (response) {
-                await storage.set("loggedInUser", JSON.stringify(response));
-                setUser(response);
-                authService.setToken(response.token);
-            }
-        } catch (error: AxiosError | any) {
-            console.error(error.code, error.message);
+  React.useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const storedUser = await storage.get("loggedInUser");
+        if (storedUser) {
+          const parsedUser = JSON.parse(storedUser) as User;
+          authService.setToken(parsedUser.token);
+          console.log(parsedUser.token)
+          setUser(parsedUser);
         }
+      } catch (error) {
+        // do nothing
+      }
     };
 
-    const signOut = async () => {
-        await storage.remove("loggedInUser");
-        setUser(null);
-    };
+    checkAuth();
+  }, []);
 
-    const service = {
-        signIn,
-        signOut,
-        user,
-    };
+  const signIn = async (provider: string, token: string) => {
+    try {
+      const response = await authService.signIn(provider, token);
+      if (response) {
+        await storage.set("loggedInUser", JSON.stringify(response));
+        setUser(response);
+        authService.setToken(response.token);
+      }
+    } catch (error: AxiosError | any) {
+      console.error(error.code, error.message);
+    }
+  };
 
-    return (
-        <AuthContext.Provider value={service}>
-            {children}
-        </AuthContext.Provider>
-    );
+  const signOut = async () => {
+    await storage.remove("loggedInUser");
+    setUser(null);
+  };
+
+  const service = {
+    signIn,
+    signOut,
+    user,
+  };
+
+  return (
+    <AuthContext.Provider value={service}>{children}</AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => React.useContext(AuthContext);
