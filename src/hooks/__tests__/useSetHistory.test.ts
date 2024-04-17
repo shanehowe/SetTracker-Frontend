75ae00685bd3ed40b657917c8f41a880465b9a9d
Shane Howe <shanehowe100@hotmail.com>
2024-04-17 22:37:11 +0100

    Add unit tests for useSetHistory hook

--- src/hooks/__tests__/useSetHistory.test.ts
+++ src/hooks/__tests__/useSetHistory.test.ts
@@ -1,7 +1,8 @@
 import setService from "../../services/sets";
 import { useSetHistory } from "../useSetHistory";
-import { renderHook } from "@testing-library/react-native";
+import { renderHook, waitFor } from "@testing-library/react-native";
 import { AllTheProviders } from "../../test-utils";
+import { SetHistory } from "../../types";
 
 describe("useSetHistory", () => {
   it("Calls setService.getSetHistory with the correct exerciseId", () => {
@@ -14,6 +15,34 @@ describe("useSetHistory", () => {
       wrapper: AllTheProviders,
     });
     expect(getSetHistory).toHaveBeenCalledWith(exerciseId);
+    unmount();
+  });
+
+  it("Returns the data from setService.getSetHistory", async () => {
+    const exerciseId = "1";
+    const data: SetHistory[] = [];
+    jest.spyOn(setService, "getSetHistory").mockReturnValue(Promise.resolve(data));
+
+    const { result, unmount } = renderHook(() => useSetHistory(exerciseId), {
+      wrapper: AllTheProviders,
+    });
+
+    await waitFor(() => expect(result.current.data).toEqual(data));
+    unmount();
+  });
+
+  it("Returns laoding, error, and isError state", async () => {
+    const exerciseId = "1";
+    const error = new Error("error");
+    jest.spyOn(setService, "getSetHistory").mockReturnValue(Promise.reject(error));
+
+    const { result, unmount } = renderHook(() => useSetHistory(exerciseId), {
+      wrapper: AllTheProviders,
+    });
+
+    expect(result.current.isLoading).toBeDefined();
+    expect(result.current.isError).toBeDefined();
+    expect(result.current.error).toBeDefined();
     unmount();
   });
 });
