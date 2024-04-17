879daaeddc43fdefc74599f1ce260159abcb98f3
Shane Howe <shanehowe100@hotmail.com>
2024-04-17 22:37:32 +0100

    Refactor common.ts to use expoConfig instead of manifest in API_URL constant

--- src/services/common.ts
+++ src/services/common.ts
@@ -2,4 +2,4 @@ import { Platform } from "react-native";
 import Constants from "expo-constants";
 
 
-export const API_URL = Platform.OS == "web" ? Constants.manifest.extra.webUrl : Constants.manifest.extra.apiUrl;
+export const API_URL = Platform.OS == "web" ? Constants.expoConfig?.extra?.webUrl : Constants.expoConfig?.extra?.apiUrl;
