82e3f7573dd5ac12c24bb02e1b048befe10f4583
Shane Howe <shanehowe100@hotmail.com>
2024-04-21 23:42:47 +0100

    Add SettingsScreen component to handle user settings

---
+++ src/screens/account/SettingsScreen.tsx
@@ -0,0 +1,54 @@
+import { StyleSheet, View } from "react-native";
+import { Divider, List, Surface, Text, useTheme } from "react-native-paper";
+import { useAuth } from "../../contexts/AuthContext";
+
+export const SettingsScreen = () => {
+  const theme = useTheme();
+  const { signOut } = useAuth();
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+        alignItems: "center",
+      }}
+    >
+      <Surface mode="flat" style={styles.surfaceStyles} elevation={5}>
+        <List.Item
+          title="Account"
+          left={(props) => <List.Icon {...props} icon={"account"} />}
+          right={(props) => <List.Icon {...props} icon={"chevron-right"} />}
+        />
+        <Divider bold={true} />
+        <List.Item
+          title="Appearance"
+          left={(props) => <List.Icon {...props} icon={"eye"} />}
+          right={(props) => <List.Icon {...props} icon={"chevron-right"} />}
+        />
+        <Divider bold={true} />
+        <List.Item
+          title="Feedback"
+          left={(props) => <List.Icon {...props} icon={"message"} />}
+          right={(props) => <List.Icon {...props} icon={"chevron-right"} />}
+        />
+      </Surface>
+      <Surface mode="flat" elevation={5} style={styles.surfaceStyles}>
+        <List.Item
+          title="Log out"
+          titleStyle={{ color: theme.colors.error, fontWeight: "bold" }}
+          left={props => <List.Icon {...props} color={theme.colors.error} icon={"logout"} />}
+          onPress={signOut}
+        />
+      </Surface>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  surfaceStyles: {
+    width: "90%",
+    padding: 8,
+    borderRadius: 8,
+    marginTop: 20
+  },
+});
