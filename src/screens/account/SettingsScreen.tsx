d5680e32c70d880cbc59adb9645c417ebdf2acf4
Shane Howe <shanehowe100@hotmail.com>
2024-04-22 19:33:10 +0100

    Refactor SettingsScreen component to use OptionsList and LogoutButton components

--- src/screens/account/SettingsScreen.tsx
+++ src/screens/account/SettingsScreen.tsx
@@ -1,10 +1,10 @@
-import { StyleSheet, View } from "react-native";
-import { Divider, List, Surface, useTheme } from "react-native-paper";
-import { useAuth } from "../../contexts/AuthContext";
+import { View } from "react-native";
+import { useTheme } from "react-native-paper";
+import { OptionsList } from "../../components/AccountSettings/OptionsList/OptionsList";
+import { LogoutButton } from "../../components/Buttons/LogoutButton/LogoutButton";
 
 export const SettingsScreen = () => {
   const theme = useTheme();
-  const { signOut } = useAuth();
   return (
     <View
       style={{
@@ -13,42 +13,8 @@ export const SettingsScreen = () => {
         alignItems: "center",
       }}
     >
-      <Surface mode="flat" style={styles.surfaceStyles} elevation={5}>
-        <List.Item
-          title="Account"
-          left={(props) => <List.Icon {...props} icon={"account"} />}
-          right={(props) => <List.Icon {...props} icon={"chevron-right"} />}
-        />
-        <Divider bold={true} />
-        <List.Item
-          title="Appearance"
-          left={(props) => <List.Icon {...props} icon={"eye"} />}
-          right={(props) => <List.Icon {...props} icon={"chevron-right"} />}
-        />
-        <Divider bold={true} />
-        <List.Item
-          title="Feedback"
-          left={(props) => <List.Icon {...props} icon={"message"} />}
-          right={(props) => <List.Icon {...props} icon={"chevron-right"} />}
-        />
-      </Surface>
-      <Surface mode="flat" elevation={5} style={styles.surfaceStyles}>
-        <List.Item
-          title="Log out"
-          titleStyle={{ color: theme.colors.error, fontWeight: "bold" }}
-          left={props => <List.Icon {...props} color={theme.colors.error} icon={"logout"} />}
-          onPress={signOut}
-        />
-      </Surface>
+      <OptionsList />
+      <LogoutButton />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  surfaceStyles: {
-    width: "90%",
-    padding: 8,
-    borderRadius: 8,
-    marginTop: 20
-  },
-});
