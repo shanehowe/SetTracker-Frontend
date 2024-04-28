b935196ddd9d926f7797ea0155d26abbee208c7f
Shane Howe <shanehowe100@hotmail.com>
2024-04-28 12:40:11 +0100

    Add users service for updating user preferences

---
+++ src/services/users.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { token } from "./auth";
+import { API_URL } from "./common";
+
+type Preferences = {
+  theme: string;
+};
+
+const updatePreferences = async (
+  updatedPreferences: Preferences
+): Promise<void> => {
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  const response = await axios.put<void>(
+    `${API_URL}/me/preferences`,
+    updatedPreferences,
+    { headers }
+  );
+  return response.data;
+};
+
+const userService = {
+  updatePreferences,
+};
+
+export default userService;
