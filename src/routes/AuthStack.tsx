99922610a5fd3afa9abc1c135656f3fad49f3006
Shane Howe <shanehowe100@hotmail.com>
2024-05-06 22:39:27 +0100

    chore: Add EmailLogInSignUpScreen to AuthStack

--- src/routes/AuthStack.tsx
+++ src/routes/AuthStack.tsx
@@ -1,18 +1,23 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { ChooseSignInMethodScreen } from '../screens/auth/ChooseSignInMethodScreen';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ChooseSignInMethodScreen } from "../screens/auth/ChooseSignInMethodScreen";
+import { EmailLogInSignUpScreen } from "../screens/auth/EmailLogInSignUpScreen";
 
 const Stack = createNativeStackNavigator();
 
 export const AuthStack = () => {
-    return (
-        <Stack.Navigator initialRouteName='SignIn'>
-            <Stack.Screen 
-                name="SignIn"
-                component={ChooseSignInMethodScreen}
-                options={{
-                    headerShown: false,
-                }}
-            />
-        </Stack.Navigator>
-    );
-}
+  return (
+    <Stack.Navigator
+      initialRouteName="SignIn"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen
+        name="SignIn"
+        component={ChooseSignInMethodScreen}
+      />
+      <Stack.Screen
+        name="EmailLogInSignUpScreen"
+        component={EmailLogInSignUpScreen}
+      />
+    </Stack.Navigator>
+  );
+};
