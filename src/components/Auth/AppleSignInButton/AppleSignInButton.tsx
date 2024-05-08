aff0f7d971982f3f84435e850bb3870d0a47ac3c
Shane Howe <shanehowe100@hotmail.com>
2024-05-08 12:18:18 +0100

    chore: Refactor AppleSignInButton component to use useSignInWithOAuthMutation hook

--- src/components/Auth/AppleSignInButton/AppleSignInButton.tsx
+++ src/components/Auth/AppleSignInButton/AppleSignInButton.tsx
@@ -1,25 +1,30 @@
-import * as AppleAuthentication from 'expo-apple-authentication';
-import { useAuth } from '../../../contexts/AuthContext';
+import * as AppleAuthentication from "expo-apple-authentication";
+import { useAuth } from "../../../contexts/AuthContext";
+import { useSignInWithOAuthMutation } from "../../../hooks/useSignInWithOAuthMutation";
 
 export const AppleSignInButton = () => {
   const auth = useAuth();
 
+  const signInOAuthMutation = useSignInWithOAuthMutation(
+    auth.onSignInSuccess,
+    console.error
+  );
+
   const handleSignIn = async () => {
     try {
       const credential = await AppleAuthentication.signInAsync({
-        requestedScopes: [
-          AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
-          AppleAuthentication.AppleAuthenticationScope.EMAIL,
-        ],
+        requestedScopes: [AppleAuthentication.AppleAuthenticationScope.EMAIL],
       });
 
       if (credential && credential.identityToken) {
-        auth.signInWithOAuth('apple', credential.identityToken);
+        signInOAuthMutation.mutate({
+          token: credential.identityToken,
+          provider: "apple",
+        });
       }
-
     } catch (e: Error | any) {
-      if (e.code === 'ERR_CANCELED') {
-        console.log('User cancelled Apple Sign in.');
+      if (e.code === "ERR_REQUEST_CANCELED") {
+        console.log("User cancelled Apple Sign in.");
       } else {
         console.log(e.code, e.message);
       }
