157ae5bf5ed57d037cce32dc21549401a5bfcaec
Shane Howe <shanehowe100@hotmail.com>
2024-05-13 13:12:36 +0100

    refactor: Update AddWorkoutFolderFAB component to adhere to code style guidelines

--- src/components/Buttons/AddWorkoutFolderFAB/AddWorkoutFolderFAB.tsx
+++ src/components/Buttons/AddWorkoutFolderFAB/AddWorkoutFolderFAB.tsx
@@ -2,23 +2,25 @@ import { FAB } from "react-native-paper";
 import { StyleSheet } from "react-native";
 
 interface AddWorkoutFolderFABProps {
-    onPress: () => void;
+  onPress: () => void;
 }
 
 export const AddWorkoutFolderFAB = ({ onPress }: AddWorkoutFolderFABProps) => {
-    return <FAB
-              icon="folder-plus"
-              onPress={onPress}
-              style={styles.fabStyle}
-              testID="add-folder-fab"
-            />;
-}
+  return (
+    <FAB
+      icon="folder-plus"
+      onPress={onPress}
+      style={styles.fabStyle}
+      testID="add-folder-fab"
+    />
+  );
+};
 
 const styles = StyleSheet.create({
-    fabStyle: {
-        position: 'absolute',
-        margin: 16,
-        bottom: 20,
-        alignSelf: 'center',
-    },
+  fabStyle: {
+    position: "absolute",
+    margin: 16,
+    bottom: 20,
+    alignSelf: "center",
+  },
 });
